729dd6833de0bab62c64508c78e7eae342d7c7bd
Pradeep Siddhartha Singh Bisen <pscrusher101@gmail.com>
2020-02-23 18:31:18 +0100

    Update Provider.java

--- webapp/src/main/webapp/Provider.java
+++ webapp/src/main/webapp/Provider.java
@@ -2,7 +2,7 @@ package bean;
   
 public interface Provider {  
 String DRIVER="oracle.jdbc.driver.OracleDriver";  
-String CONNECTION_URL="jdbc:oracle:thin:@localhost:1521:xe";  
+String CONNECTION_URL="jdbc:oracle:thin:@184.72.182.104:1521:xe";  
 String USERNAME="system";  
 String PASSWORD="oracle";  
   
