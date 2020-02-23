5413aa51e21b67d11a1fc8ad5d848bbbf0fcb221
Pradeep Siddhartha Singh Bisen <pscrusher101@gmail.com>
2020-02-23 18:32:54 +0100

    Delete Provider.java

--- webapp/src/main/webapp/Provider.java
+++
@@ -1,9 +0,0 @@
-package bean;  
-  
-public interface Provider {  
-String DRIVER="oracle.jdbc.driver.OracleDriver";  
-String CONNECTION_URL="jdbc:oracle:thin:@184.72.182.104:1521:xe";  
-String USERNAME="system";  
-String PASSWORD="oracle";  
-  
-}  
